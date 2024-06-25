6bcd8754ff5030a0092301ed303362c5a952cd3a
iAmShivay <shivaysharma77894@gmail.com>
2024-06-25 16:00:13 +0530

    Bug Fix

--- src/Backend/Server/app.js
+++ src/Backend/Server/app.js
@@ -4,19 +4,13 @@ const middleware = require("../Middleware/error");
 const cors = require('cors')
 const app = express();
 
-const product = require("../Routes/Product");
 const users = require("../Routes/user");
-const order = require("../Routes/order");
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/v1", product);
 app.use("/api/v1", users);
-app.use("/api/v1", order);
 
 app.use(middleware);
 
 module.exports = app;
-
-// middleware for errorHandling
