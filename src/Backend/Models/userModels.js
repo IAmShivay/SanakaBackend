c885c5d5a915a9f2f20ce7ef148f76a0f0e0abd9
iAmShivay <shivaysharma77894@gmail.com>
2024-06-26 12:57:24 +0530

    error fixed

--- src/Backend/Models/userModels.js
+++ src/Backend/Models/userModels.js
@@ -52,7 +52,7 @@ userSchema.pre("save", async function (next) {
 // JWT TOKEN
 userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET||'JISHIDWJWKHKWAHDJKAKJWH', {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: process.env.JWT_EXPIRE || '5d',
   });
 };
 
